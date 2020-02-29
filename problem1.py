e2570655c551b1cb6302c6bdb00e6350feebb7bf
Chaithrashetty89 <61625050+Chaithrashetty89@users.noreply.github.com>
2020-02-29 20:04:42 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,7 @@
+#PROBLEM 1
+name= input("enter name:")
+age= int(input("enter age:"))
+current_year = 2020
+birth_year= 2020 - age 
+hundredth_year = birth_year + 100
+print("Hey {} ! you will turn 100 years old in {}" .format(name, hundredth_year))
